93a5bcc0bb01f5a2295eb9ae2645c555448d0e1d
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-10-23 16:03:18 -0300

    ci: add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION'){
+      config.base = '/design-system/'
+    }
+    return config
   }
 }
