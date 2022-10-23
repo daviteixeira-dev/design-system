c58621880e93123579392dc0d345680cdff579ee
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-10-23 15:10:54 -0300

    do figma ao react, design system do projeto com storybook

---
+++ tailwind.config.cjs
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32
+    },
+
+    colors: {
+
+      transparent: 'transparent',
+
+      black: '#000',
+      white: '#fff',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+
+      cyan: {
+        500: '#81d8f7',
+        300: '#9be1f8',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}
